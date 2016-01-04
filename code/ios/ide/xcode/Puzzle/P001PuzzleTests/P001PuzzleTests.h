4eb63d759087c6a2c0ae4c18fe5445d5c5b6de24
梦宇@极客学院 <xtiqin@163.com>
2016-01-04 10:43:35 +0800

    Nil

--- code/ios/ide/xcode/Puzzle/P001PuzzleTests/P001PuzzleTests.h
+++ code/ios/ide/xcode/Puzzle/P001PuzzleTests/P001PuzzleTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface P001PuzzleTests : SenTestCase
+@interface P001PuzzleTests : XCTestCase
 
 @end
